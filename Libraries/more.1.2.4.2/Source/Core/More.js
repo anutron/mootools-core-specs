6ef1f41da1167c2fa514264652273af0c999c10b
Aaron Newton <git@clientcide.com>
2009-12-21 23:08:10 -0800

    adding all the mootools -core and -more tagged versions.

---
+++ Libraries/more.1.2.4.2/Source/Core/More.js
@@ -0,0 +1,26 @@
+/*
+---
+
+script: More.js
+
+description: MooTools More
+
+license: MIT-style license
+
+authors:
+- Guillermo Rauch
+- Thomas Aylott
+- Scott Kyle
+
+requires:
+- core:1.2.4/MooTools
+
+provides: [MooTools.More]
+
+...
+*/
+
+MooTools.More = {
+	'version': '1.2.4.2',
+	'build': 'bd5a93c0913cce25917c48cbdacde568e15e02ef'
+};
