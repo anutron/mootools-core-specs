d9b863e3b7cc0c69cc9e82910c55bd74dec71bc3
Aaron Newton <git@clientcide.com>
2009-12-29 16:23:34 -0800

    adding / updating references to config files for the 1.1 public tests

--- Specs/1.1public/UserTests/config.js
+++ Specs/1.1public/UserTests/config.js
@@ -1,10 +1,10 @@
 var UnitTester = {
 	site: 'MooTools 1.11', //title of your site
 	title: 'Unit Tests', //title of this test group
-	path: '../../Tests/UnitTester/',
+	path: '../../../Tests/UnitTester/',
 	ready: function(UnitTester){
 		new UnitTester({
-			core: '../../../' //path to Source/scripts.json
+			core: '../../../Libraries/core.1.1.2/scripts.json' //path to Source/scripts.json
 		}, {
 			DemoScripts: 'Tests/' //path to tests.json
 		}, {
